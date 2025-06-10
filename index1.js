c3cee24c946f99f7c9992661c7b97cdeaaaff28d
eppoteck <rifqirifky2@gmail.com>
2025-06-10 10:35:07 +0700

    Add files via upload

---
+++ index1.js
@@ -0,0 +1,20 @@
+const siswa = [
+    {
+        productName: 'luna',
+        productNumber: '100',
+        paymentStatus: 'sudah',
+        status: 'Active'
+    },
+    {
+        productName: 'faren',
+        productNumber: '50',
+        paymentStatus: 'sudah',
+        status: 'Active'
+    },
+    {
+        productName: 'ardani',
+        productNumber: '50',
+        paymentStatus: 'sudah',
+        status: 'Active'
+    },
+]
